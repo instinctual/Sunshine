79d6ccd67c3ec295c3ba9f0bdf9c6da05c063f60
Alan Latteri <alan@instinctual.la>
2026-08-23 14:54:09 -0700

    web: apply StationConnect host branding

--- src_assets/common/assets/web/NavbarSimple.vue
+++ src_assets/common/assets/web/NavbarSimple.vue
@@ -1,8 +1,8 @@
 <template>
   <nav class="navbar navbar-sunshine">
     <div class="container-fluid">
-      <span class="navbar-brand" title="Sunshine">
-        <img src="/images/logo-sunshine-45.png" height="45" alt="Sunshine">
+      <span class="navbar-brand" title="StationConnect Host">
+        StationConnect Host
       </span>
       <ThemeToggle/>
     </div>
